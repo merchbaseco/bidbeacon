3ccc2f028d5ea219232dff3ec129a1cb0dc835b6
Zach Knickerbocker <zknicker@gmail.com>
2025-12-13 23:38:05 -0500

    Fix migration 14.

--- drizzle/0014_friendly_killer_shrike.sql
+++ drizzle/0014_friendly_killer_shrike.sql
@@ -1,3 +1,3 @@
--- Set all existing report_id values to null
-UPDATE "report_dataset_metadata" SET "report_id" = NULL;--> statement-breakpoint
-ALTER TABLE "report_dataset_metadata" ALTER COLUMN "report_id" DROP NOT NULL;
+-- Drop NOT NULL constraint first, then update values to null
+ALTER TABLE "report_dataset_metadata" ALTER COLUMN "report_id" DROP NOT NULL;--> statement-breakpoint
+UPDATE "report_dataset_metadata" SET "report_id" = NULL WHERE "report_id" IS NOT NULL;
